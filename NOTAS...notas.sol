0f7974ac5a57867afa56c5ccd0109796bf982d7f
Yesid Rocha Novoa <yern86@gmail.com>
2022-03-05 17:33:16 -0500

    Actualización en Funciones
    Se actualizan funciones, para que no solo se asigne nota a un estudiante, si no que se asigne nota a una asignatura especifica.

--- NOTAS...notas.sol
+++ NOTAS...notas.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.4 < 0.7.0;
+pragma solidity >=0.4.22 <0.9.0;
 pragma experimental ABIEncoderV2;
 
 // -----------------------------------
 //  ALUMNO   |    ID    |      NOTA
 // -----------------------------------
 //  Marcos |    77755N    |      5
-//  Yesid  |    12345X    |      9
+//  Joan   |    12345X    |      9
 //  Maria  |    02468T    |      2
 //  Marta  |    13579U    |      3
-//  Abi    |    98765Z    |      5
+//  Alba   |    98765Z    |      5
 
 contract notas {
     
@@ -24,17 +24,18 @@ contract notas {
     // Mapping para relacionar el hash de la identidad del alumno con su nota del examen
     mapping (bytes32 => uint) Notas;
     
-    // Array de los alumnos de pidan revisiones de examen
-    string [] revisiones;
+    // Mapping de los alumnos de pidan revisiones de una asignatura
+    mapping (string => string []) revisiones;
+    
     
     // Eventos 
     event alumno_evaluado(bytes32);
     event evento_revision(string);
     
     // Funcion para evaluar a alumnos
-    function Evaluar(string memory _idAlumno, uint _nota) public UnicamenteProfesor(msg.sender){
+    function Evaluar(string memory _asignatura, string memory _idAlumno, uint _nota) public UnicamenteProfesor(msg.sender){
         // Hash de la identificacion del alumno 
-        bytes32 hash_idAlumno = keccak256(abi.encodePacked(_idAlumno));
+        bytes32 hash_idAlumno = keccak256(abi.encodePacked(_asignatura, _idAlumno));
         // Relacion entre el hash de la identificacion del alumno y su nota
         Notas[hash_idAlumno] = _nota;
         // Emision del evento
@@ -49,9 +50,9 @@ contract notas {
     }
     
     // Funcion para ver las notas de un alumno 
-    function VerNotas(string memory _idAlumno) public view returns(uint) {
+    function VerNotas(string memory _asignatura, string memory _idAlumno) public view returns(uint) {
         // Hash de la identificacion del alumno 
-        bytes32 hash_idAlumno = keccak256(abi.encodePacked(_idAlumno));
+        bytes32 hash_idAlumno = keccak256(abi.encodePacked(_asignatura, _idAlumno));
         // Nota asociada al hash del alumno
         uint nota_alumno = Notas[hash_idAlumno];
         // Visualizar la nota 
@@ -59,17 +60,17 @@ contract notas {
     } 
     
     // Funcion para pedir una revision del examen
-    function Revision(string memory _idAlumno) public {
+    function Revision(string memory _asignatura, string memory _idAlumno) public {
         // Almacenamiento de la identidad del alumno en un array
-        revisiones.push(_idAlumno);
+        revisiones[_asignatura].push(_idAlumno);
         // Emision del evento 
         emit evento_revision(_idAlumno);
     }
     
     // Funcion para ver los alumnos que han solicitado revision de examen
-    function VerRevisiones() public view UnicamenteProfesor(msg.sender) returns (string [] memory){
+    function VerRevisiones(string memory _asignatura) public view UnicamenteProfesor(msg.sender) returns (string [] memory){
         // Devolver las identidades de los alumnos
-        return revisiones;
+        return revisiones[_asignatura];
     }
     
 }
